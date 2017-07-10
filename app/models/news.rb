1ff780447551ce97c5d2b79cfc38c6faaac6c4c5
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-07-10 17:05:25 +0000

    Adding migration and modeling enum

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,5 @@
 class News < ApplicationRecord
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710164302) do
+ActiveRecord::Schema.define(version: 20170710164949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,9 +55,10 @@ ActiveRecord::Schema.define(version: 20170710164302) do
     t.string   "title"
     t.text     "subtitle"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_news_on_slug", unique: true, using: :btree
   end
 
