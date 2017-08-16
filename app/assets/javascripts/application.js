34d691291071bd9a8bff8bd0e3481a7f358c4e49
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-08-16 13:59:48 +0000

    Implemented styles for nav_bar, footer, about pages - start of refactoring added files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,19 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-//= require_tree .
+//= require modernizr-2.6.2-respond-1.1.0.min
+//= require jquery.easing.1.3
+//= require bootstrap.min
+//= require jquery.isotope.min
+//= require jquery.nicescroll.min
+//= require jquery.scrollTo-1.4.3.1-min
+//= require jquery.localscroll-1.2.7-min
+//= require stellar
+//= require jquery.appear
+//= require validate
+//= require grid
+//= require wow.min
+//= require skrollr.min
