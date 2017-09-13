fc561bb64d890137e3b7f2a9daa95fdc289e1bd6
Jordan Hudgens <wernerfosspilot@gmail.com>
2017-09-13 15:36:51 +0000

    Finished with setting AWS (Fuck Ohio)

--- app/uploaders/language_uploader.rb
+++ app/uploaders/language_uploader.rb
@@ -39,7 +39,7 @@ class LanguageUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
-
+  
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
